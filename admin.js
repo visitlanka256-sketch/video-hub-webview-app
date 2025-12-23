225f1e48a2108fc7975d6ee6b52b2777decf0d9e
visitlanka256-sketch <visitlanka256@gmail.com>
2025-12-23 19:10:32 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,1 +1,13 @@
 
+function addVideo(){
+ let v={
+  id:Date.now(),
+  title:title.value,
+  url:url.value,
+  type:type.value
+ };
+ let vids=JSON.parse(localStorage.getItem("videos"))||[];
+ vids.unshift(v);
+ localStorage.setItem("videos",JSON.stringify(vids));
+ alert("Video Added");
+}
